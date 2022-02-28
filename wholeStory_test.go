43e0e73b918f846a4ac68e348cdb88d6515de347
octavio-luna <octavioluna2003@gmail.com>
2022-02-28 17:09:55 -0300

    test wholeStory OK

--- wholeStory_test.go
+++ wholeStory_test.go
@@ -1,1 +1,24 @@
 package main
+
+import "testing"
+
+func TestWholeStory(t *testing.T) {
+	tables := []struct {
+		s      string
+		result string
+	}{
+		{"12-as-23-qw", " as qw"},
+		{"as-12-as-23-qw", "as as qw"},
+		{"", ""},
+		{" as-qw", " as qw"},
+		{"12-sdfsd-23-11134", " sdfsd"},
+		{"1fg-12-sdf-113", " fg sdf"},
+		{".sad234.3-234-as", " sad as"},
+	}
+	for _, table := range tables {
+		res := wholeStory(table.s)
+		if res != table.result {
+			t.Errorf("error %s got %s, expected %s", table.s, res, table.result)
+		}
+	}
+}
