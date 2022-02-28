2e74d52601b47c7f930fbb65157167d6ab56d9b6
octavio-luna <octavioluna2003@gmail.com>
2022-02-28 14:07:53 -0300

    initial commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+/*
+STRING SPECS
+
+Given a string that is a sequence of numbers followed by dash followed by text, eg: 23-ab-48-caba-56-haha
+
+-The numbers can be assumed to be unsigned integers
+-The strings can be assumed to be ASCII sequences of arbitrary length larger than 0 (empty strings not allowed).
+
+TASK 1
+
+-Before implementing the needed function, please estimate the difficulty of the functions and report the
+estimated and the used time to implement the functions in the function comment block.
+
+-Write a function testValidity that takes the string as an input, and returns boolean flag true if the given
+string complies with the format, or false if the string does not comply
+
+-Write a function averageNumber that takes the string, and returns the average number from all the numbers
+
+-Write a function wholeStory that takes the string, and returns a text that is composed from all the text
+ words separated by spaces, e.g. story called for the string 1-hello-2-world should return text: "hello world"
+
+ -Write a function storyStats that returns four things:
+	-the shortest word
+	-the longest word
+	-the average word length
+	-the list (or empty list) of all words from the story that have the length the same as the average length rounded up and down.
+*/
+
+//-Write a function testValidity that takes the string as an input, and returns boolean flag true if the given
+//string complies with the format, or false if the string does not comply
+
+//Estimated difficulty testValidity: low. Estimated time: 15 minutes
+
+func main() {
+}
